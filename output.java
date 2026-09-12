aa7fc50998000665a45dd48e5ee3b7f2d41de0de
Shreya Sharma <shreyasharmajsr22@gmail.com>
2026-09-12 21:07:26 +0530

    Add simple menu system in output.java

---
+++ output.java
@@ -0,0 +1,15 @@
+---SIMPLE MENU SYSTEM---
+choose a category
+1.Food
+2.Drink
+enter your choice(1 or 2):
+1
+
+Food options:
+1.Pizza
+2.Burger
+Enter your choice(1 or 2):
+1
+You have selected pizza!
+Price: $12.99
+Thank you for using the menu system!
